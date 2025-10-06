15b92008bfdb940d71b4a41961c25fcc223b7bd0
dammarin <marinatosd@gmail.com>
2025-10-06 21:12:05 +0300

    feat(todo): implement todo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Layout from "./components/layout/Layout.tsx";
+import Todo from "./components/todo/Todo.tsx";
 
 function App() {
 
   return (
     <>
+      <Layout>
+        <Todo/>
+      </Layout>
     </>
   )
 }
